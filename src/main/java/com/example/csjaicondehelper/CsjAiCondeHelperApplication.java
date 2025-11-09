b098809a169135eedc447a204eb6b939f41416d0
SuperMaxJia <cuisijia@example.com>
2025-11-09 20:38:49 +0800

    feat:初始创建

---
+++ src/main/java/com/example/csjaicondehelper/CsjAiCondeHelperApplication.java
@@ -0,0 +1,13 @@
+package com.example.csjaicondehelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsjAiCondeHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsjAiCondeHelperApplication.class, args);
+    }
+
+}
